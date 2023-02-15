9b3ac7dcde839f356741fb40b368d18e1a7fdfdf
Trần Quang Lại <89112954+girlkun75@users.noreply.github.com>
2023-02-15 15:03:06 +0700

    Commit

--- src/main/java/com/snackman/datnud11/controller/CategoryController.java
+++ src/main/java/com/snackman/datnud11/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.snackman.datnud11.controller;
 
+import com.snackman.datnud11.dto.CategoryDTO;
 import com.snackman.datnud11.entity.Category;
 import com.snackman.datnud11.repo.CategoryRepository;
 import com.snackman.datnud11.repo.RoleEmployeeRepository;
+import com.snackman.datnud11.services.CategoryService;
 import com.snackman.datnud11.services.RoleEmployeeService;
 import com.snackman.datnud11.utils.customException.CustomNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +18,23 @@ import java.util.List;
 @RequestMapping("/api/v1/categorys")
 public class CategoryController {
     @Autowired
-    CategoryRepository categoryRepository;
+    private CategoryService categoryService;
     
     @GetMapping
-    public ResponseEntity<List<Category>> getRolesEmployee(){
-        return new ResponseEntity<>(categoryRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<Category>> getCategories(){
+        return new ResponseEntity<>(this.categoryService.findAll(), HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<Category> createCategory(@RequestBody Category category){
-        return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.CREATED);
+    public ResponseEntity<Category> createCategory(@RequestBody CategoryDTO categoryDTO){
+        return new ResponseEntity<>(this.categoryService.save(new Category(categoryDTO)), HttpStatus.CREATED);
     }
     @PutMapping
-    public ResponseEntity<Category> updateRolesEmployeeById(@RequestBody Category category) throws CustomNotFoundException {
-        
-        return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.CREATED);
+    public ResponseEntity<Category> updateCategory(@RequestBody CategoryDTO categoryDTO) throws CustomNotFoundException {
+        return new ResponseEntity<>(this.categoryService.save(new Category(categoryDTO)), HttpStatus.CREATED);
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCatrgoryById(@PathVariable(name = "id") Long id) throws CustomNotFoundException{       
-        return new ResponseEntity<>("Delete Successfully!",HttpStatus.NO_CONTENT);
+    public ResponseEntity<String> deleteCatrgoryById(@PathVariable(name = "id") Long id) throws CustomNotFoundException{
+        this.categoryService.delete(id);
+        return ResponseEntity.ok("Delete successfully!");
     }
 }
---
+++ src/main/java/com/snackman/datnud11/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.snackman.datnud11.services;
+
+import com.snackman.datnud11.entity.Category;
+import com.snackman.datnud11.entity.Colors;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    Category save(Category category);
+
+    void delete(Category category);
+
+    void delete(Long id);
+
+    Category findById(Long id) throws Exception;
+
+    List<Category> findAll();
+}
