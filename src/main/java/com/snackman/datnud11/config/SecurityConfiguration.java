4b53c801d5c600c3841c6265b8c655011622ccb3
tvu47 <tvu76165@gmail.com>
2023-03-08 21:32:43 +0700

    update cor config

--- src/main/java/com/snackman/datnud11/config/SecurityConfiguration.java
+++ src/main/java/com/snackman/datnud11/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
         .csrf()
             .disable()
         .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**","/api/v1/products")
+        .requestMatchers("/api/v1/auth/**","/api/v1/product")
           .permitAll()
         .anyRequest()
           .authenticated()
