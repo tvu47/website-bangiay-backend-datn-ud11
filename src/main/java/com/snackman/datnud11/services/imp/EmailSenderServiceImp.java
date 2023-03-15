283096b8ebea6ccc8745039146a647e101ba9787
tvu47 <tvu76165@gmail.com>
2023-03-16 00:25:53 +0700

    night

---
+++ src/main/java/com/snackman/datnud11/services/imp/EmailSenderServiceImp.java
@@ -0,0 +1,23 @@
+package com.snackman.datnud11.services.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderServiceImp {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("snackman1747@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+
+        System.out.println("mail sent successfully...");
+    }
+}
