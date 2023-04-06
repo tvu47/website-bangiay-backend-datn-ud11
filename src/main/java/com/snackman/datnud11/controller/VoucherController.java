d16a5752b8bcf325de3a127221c82b21bc54434b
Trần Quang Lại <89112954+girlkun75@users.noreply.github.com>
2023-04-06 14:15:23 +0700

    .

--- src/main/java/com/snackman/datnud11/controller/VoucherController.java
+++ src/main/java/com/snackman/datnud11/controller/VoucherController.java
@@ -1,13 +1,14 @@
 package com.snackman.datnud11.controller;
 
+import com.snackman.datnud11.dto.VoucherDTO;
+import com.snackman.datnud11.entity.Voucher;
+import com.snackman.datnud11.responses.NoticeResponse;
 import com.snackman.datnud11.responses.VoucherResponse;
 import com.snackman.datnud11.services.VoucherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +22,17 @@ public class VoucherController {
     @GetMapping("/available")
     public ResponseEntity<List<VoucherResponse>> getVoucherAvailable(){
         return new ResponseEntity<>(this.service.findAllAvailable(), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<VoucherResponse>> getAllVouchers(){
+        return new ResponseEntity<>(this.service.findAllVouchers(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<NoticeResponse> addNewVoucher(@RequestBody VoucherDTO voucherDTO) throws Exception {
+        VoucherResponse voucher = this.service.save(voucherDTO);
+        return new ResponseEntity<>(new NoticeResponse(HttpStatus.OK.value(), "Thêm mới thành công!", voucher), HttpStatus.OK);
     }
 
 }
