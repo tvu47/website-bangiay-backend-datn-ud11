487824817ebb0e2e617f9aed1a6e0ae351225478
tvu47 <tvu76165@gmail.com>
2023-04-24 00:06:39 +0700

    fix error

--- src/main/java/com/snackman/datnud11/repo/BillRepository.java
+++ src/main/java/com/snackman/datnud11/repo/BillRepository.java
@@ -27,17 +27,18 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
 
     List<Bill> findAllBillByCustomerId(@Param("id") Long id);
 
-    @Query(value = "select * from bill where customer_id = :id and status=:statuss order by create_time desc", nativeQuery = true)
+    @Query(value = "select * from bill where status=:statuss order by create_time desc", nativeQuery = true)
     List<Bill> findBillByStatus(@Param("statuss") int status);
     @Query(value = "select * from bill where status=:statuss and create_time between :begin and :to", nativeQuery = true)
     List<Bill> thongKeHoaDon(@Param("begin") @Temporal(TemporalType.TIMESTAMP) Date startDate,
                              @Param("to") @Temporal(TemporalType.TIMESTAMP) Date endDate,
                              @Param("statuss") int status);
 
-    @Query(value = "SELECT new com.snackman.datnud11.responses.Count(COUNT(b.id), SUM(b.totalPrice)) FROM Bill b where b.createTime between :begin and :to")
+    @Query(value = "SELECT new com.snackman.datnud11.responses.Count(COUNT(b.id), SUM(b.totalPrice)) FROM Bill b where b.status=:statuss and b.createTime between :begin and :to")
     Count tinhTong(@Param("begin") @Temporal(TemporalType.TIMESTAMP) Date startDate,
-                   @Param("to") @Temporal(TemporalType.TIMESTAMP) Date endDate);
+                   @Param("to") @Temporal(TemporalType.TIMESTAMP) Date endDate,
+                   @Param("statuss") int status);
 
-    @Query(value = "SELECT new com.snackman.datnud11.responses.Count(COUNT(b.id), SUM(b.totalPrice)) FROM Bill b")
-    Count tinhTongAll();
+    @Query(value = "SELECT new com.snackman.datnud11.responses.Count(COUNT(b.id), SUM(b.totalPrice)) FROM Bill b WHERE b.status=:statuss")
+    Count tinhTongAll(@Param("statuss") int status);
 }
